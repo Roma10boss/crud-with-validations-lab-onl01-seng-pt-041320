95d51fa924d4ddecacef2580a46050a9673a4893
Romario Gustave <romagustave10@gmail.com>
2020-09-15 03:38:03 +0000

    Automatically backed up by Learn

--- db/migrate/20200915033743_create_song.rb
+++ db/migrate/20200915033743_create_song.rb
@@ -1,6 +1,14 @@
 class CreateSong < ActiveRecord::Migration[5.0]
   def change
-    create_table :songs do |t|
-    end
-  end
+    def change
+   create_table :songs do |t|
+     t.string :title
+     t.string :artist_name
+     t.integer :release_year
+     t.boolean :released
+     t.string :genre
+
+     t.timestamps
+   end
+ end
 end
